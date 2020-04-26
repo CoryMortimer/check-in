02d3149cbf26fefe1ac35e296ce58a4511b13cde
Cory Mortimer <cory.m.mortimer@gmail.com>
2020-04-26 09:22:04 -0700

    Add privacy policy and terms of service

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
 import Grid from '../ui/Grid';
 import TitleBar from '../ui/NavBars/TitleBar';
 
@@ -12,6 +13,9 @@ const Login = () => (
       </Grid>
       <Grid item xs={12} align="center">
         <Button href={`${process.env.REACT_APP_SERVER_DOMAIN}/session/authenticate`}>Start Here</Button>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Typography variant="body">By using our service, you agree to our <Link to="/terms">Terms and Conditions</Link> and <Link to="/privacy">Privacy Policy</Link>.</Typography>
       </Grid>
     </Grid>
   </TitleBar>
--- frontend/src/components/Router/Router.jsx
+++ frontend/src/components/Router/Router.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from '../Login';
 import GroupsRouter from '../Groups/GroupsRouter';
 import ProtectedRoute from './ProtectedRoute';
 import UnprotectedRoute from './UnprotectedRoute';
+import TermsOfService from '../TermsOfService';
+import PrivacyPolicy from '../PrivacyPolicy';
 
 const Router = () => (
   <Container maxWidth="md">
     <BrowserRouter>
       <Switch>
         <ProtectedRoute path="/groups" component={GroupsRouter} />
+        <Route path="/terms" component={TermsOfService} />
+        <Route path="/privacy" component={PrivacyPolicy} />
         <UnprotectedRoute exact path="/" component={Login} />
       </Switch>
     </BrowserRouter>
